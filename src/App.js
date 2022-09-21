24c7391bda3c3cfc8d571597f9698ed46917a13f
Lauren-Nicole <popich.red@gmail.com>
2022-09-21 13:39:51 +0000

    Import & route contact component - app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Container } from 'react-bootstrap';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route exact path="/experience" render={() => <h1>EXPERIENCE</h1>} />
         <Route exact path="/location" render={() => <h1>LOCATION</h1>} />
         <Route exact path="/gallery" render={() => <h1>GALLERY</h1>} />
-        <Route exact path="/contact" render={() => <h1>CONTACT</h1>} />
+        <Route exact path="/contact" render={() => <Contact />} />
         <Route render={() => <h3>Page Not Found.</h3>} />
       </Switch>
     </Container>
