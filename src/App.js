b39940f1615c1de0e3cd42e41f146c2b85e3e3a6
Lauren-Nicole <popich.red@gmail.com>
2022-09-21 12:19:10 +0000

    Import & route NavBar component - App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 /* eslint-disable */
 import { Container } from 'react-bootstrap';
 import { Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Container>
+      <NavBar />
       <Switch>
         <Route exact path="/" render={() => <h1>HELLO WORLD</h1>} />
-        <Route exact path="/" render={() => <h1>EXPERIENCE</h1>} />
-        <Route exact path="/" render={() => <h1>LOCATION</h1>} />
-        <Route exact path="/" render={() => <h1>GALLERY</h1>} />
-        <Route exact path="/" render={() => <h1>CONTACT</h1>} />
+        <Route exact path="/experience" render={() => <h1>EXPERIENCE</h1>} />
+        <Route exact path="/location" render={() => <h1>LOCATION</h1>} />
+        <Route exact path="/gallery" render={() => <h1>GALLERY</h1>} />
+        <Route exact path="/contact" render={() => <h1>CONTACT</h1>} />
         <Route render={() => <h3>Page Not Found.</h3>} />
       </Switch>
     </Container>
