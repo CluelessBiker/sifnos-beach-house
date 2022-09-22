54d126f99e6094ccf8cb776612d180ea969056b4
Lauren-Nicole <popich.red@gmail.com>
2022-09-22 12:18:09 +0000

    Import & route neighborhood component - App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Contact from './pages/Contact';
 import styles from './App.module.css';
+import Neighborhood from './pages/Neighborhood';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" render={() => <h1>HELLO WORLD</h1>} />
         <Route exact path="/experience" render={() => <h1>EXPERIENCE</h1>} />
-        <Route exact path="/location" render={() => <h1>LOCATION</h1>} />
+        <Route exact path="/neighborhood" render={() => <Neighborhood />} />
         <Route exact path="/gallery" render={() => <h1>GALLERY</h1>} />
         <Route exact path="/contact" render={() => <Contact />} />
         <Route render={() => <h3>Page Not Found.</h3>} />
