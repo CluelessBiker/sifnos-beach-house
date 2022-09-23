41f663d242c8536a689f6a61848e32aab3478464
Lauren-Nicole <popich.red@gmail.com>
2022-09-23 11:44:35 +0000

    Remove bootstrap container - neighborhood.js

--- src/pages/Neighborhood.js
+++ src/pages/Neighborhood.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import styles from '../styles/Neighborhood.module.css';
 
 function Neighborhood () {
     return (
-        <Container>
+        <div>
             <iframe
                 className={styles.GoogleMap}
                 id="gmap"
                 title="Platys Gialos, Syros"
                 src="https://maps.google.com/maps?q=platys%20gialos%20sifnos&t=&z=15&ie=UTF8&iwloc=&output=embed"
             ></iframe>
-        </Container>
+        </div>
 
     )
 }
