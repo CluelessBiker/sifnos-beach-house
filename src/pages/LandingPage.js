f08b551ee6a1ce1865732db16c5670a23a53f992
Lauren-Nicole <popich.red@gmail.com>
2022-09-23 10:17:10 +0000

    Import css file - LP.js

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import styles from '../styles/LandingPage.module.css';
 
 function LandingPage() {
     return (
         <Container>
-
+            <div className={styles.HeroImage}></div>
         </Container>
     )
 }
