9d65fd9407873550d2bdb4e72e60b2ca91f9ddb0
Lauren-Nicole <popich.red@gmail.com>
2022-09-26 07:51:52 +0000

    Create accomodation page component.

---
+++ src/pages/Accommodation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HouseRules from '../components/HouseRules';
+
+function Accommodation(){
+    return(
+        <div>
+            <HouseRules />
+        </div>
+    )
+}
+
+export default Accommodation;
