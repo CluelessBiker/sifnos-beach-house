0ba29d0b0fb1827f7bc12866fec306a3a89d84b5
Lauren-Nicole <popich.red@gmail.com>
2022-09-26 08:02:53 +0000

    Populate House Rules component.

--- src/components/HouseRules.js
+++ src/components/HouseRules.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 function HouseRules() {
     return (
-        <div>TEST</div>
+        <div>
+            <h3>Things to know</h3>
+            <Row>
+                <Col xs={12} sm={4}>
+                    <h4>House Rules:</h4>
+                    <p>Check-in: After 11:00</p>
+                    <p>Check-out: Before 13:00</p>
+                    <p>No Smoking</p>
+                    <p>No Pets</p>
+                    <p>No parties/events</p>
+                </Col>
+
+                <Col xs={12} sm={4}>
+                    <h4>Health & Safety:</h4>
+                    <p>Carbon Monoxide Detector: No</p>
+                    <p>Smoke Alarm: No</p>
+                </Col>
+
+                <Col xs={12} sm={4}>
+                    <h4>Cancellation Policy:</h4>
+                    <p>Full refund within 48h from the time the reservation was made.</p>
+                    <p>50% refund with two weeks notice.</p>
+                    <p>No refund.</p>
+                </Col>
+            </Row>
+        </div>
     )
 }
 
