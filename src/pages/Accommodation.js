c29f8da6f18a882f4b57ddaed7ebcf376aa492e0
Lauren-Nicole <popich.red@gmail.com>
2022-09-28 06:28:42 +0000

    Implement image layout - accommodation.js

--- src/pages/Accommodation.js
+++ src/pages/Accommodation.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import Amenities from '../components/Amenities';
 import HouseRules from '../components/HouseRules';
 
 function Accommodation(){
     return(
         <div>
+            <Row>
+                <Col md={6}>
+                    <div></div>
+                </Col>
+                <Col md={6}>
+                    <Row>
+                        <div></div>
+                    </Row>
+                    <Row>
+                        <Col md={6}>
+                            <div></div>
+                        </Col>
+                        <Col md={6}>
+                            <div></div>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
             <Amenities />
             <HouseRules />
         </div>
