16d9441d98b4d78715c40060c3b055a8262c6188
Lauren-Nicole <popich.red@gmail.com>
2022-09-28 06:33:22 +0000

    Import style sheet & assign classnames to divs - accommodation.js

--- src/pages/Accommodation.js
+++ src/pages/Accommodation.js
@@ -2,24 +2,25 @@ import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import Amenities from '../components/Amenities';
 import HouseRules from '../components/HouseRules';
+import styles from '../styles/Accommodation.module.css'
 
 function Accommodation(){
     return(
         <div>
             <Row>
                 <Col md={6}>
-                    <div></div>
+                    <div className={styles.Image}></div>
                 </Col>
                 <Col md={6}>
                     <Row>
-                        <div></div>
+                        <div className={styles.Image}></div>
                     </Row>
                     <Row>
                         <Col md={6}>
-                            <div></div>
+                            <div className={styles.Image}></div>
                         </Col>
                         <Col md={6}>
-                            <div></div>
+                            <div className={styles.Image}></div>
                         </Col>
                     </Row>
                 </Col>
