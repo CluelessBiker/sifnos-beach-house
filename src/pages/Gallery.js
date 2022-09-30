6e15b074d3058b0484e6556740a3effa0c7398f4
Lauren-Nicole <popich.red@gmail.com>
2022-09-30 09:39:50 +0000

    Add basic photo layout - gallery.js

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,8 +1,80 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import styles from '../styles/Accommodation.module.css';
 
 function Gallery() {
     return (
+        <div>
+            <Row>
+                <Col md={6} className={styles.ImageCol}>
+                    <div className={styles.ImageLG}></div>
+                </Col>
+                <Col md={6}>
+                    <Row>
+                        <Col md={12} className={styles.ImageCol}>
+                            <div className={styles.ImageMD}></div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={6} className={styles.ImageCol}>
+                            <div className={styles.ImageSM}></div>
+                        </Col>
+                        <Col md={6} className={styles.ImageCol}>
+                            <div className={styles.ImageSM}></div>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
 
+            <Row>
+                
+                <Col md={6}>
+                <Row>
+                    <Col md={6} className={styles.ImageCol}>
+                        <div className={styles.ImageSM}></div>
+                    </Col>
+                    <Col md={6} className={styles.ImageCol}>
+                        <div className={styles.ImageSM}></div>
+                    </Col>
+                    </Row>
+                    <Row>
+                        <Col md={12} className={styles.ImageCol}>
+                            <div className={styles.ImageMD}></div>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col md={6} className={styles.ImageCol}>
+                    <div className={styles.ImageLG}></div>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col md={6} className={styles.ImageCol}>
+                    <div className={styles.ImageLG}></div>
+                </Col>
+                <Col md={6}>
+                    <Row>
+                        <Col md={12} className={styles.ImageCol}>
+                            <div className={styles.ImageMD}></div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={6} className={styles.ImageCol}>
+                            <div className={styles.ImageSM}></div>
+                        </Col>
+                        <Col md={6} className={styles.ImageCol}>
+                            <div className={styles.ImageSM}></div>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            
+        </div>
     )
 }
 
