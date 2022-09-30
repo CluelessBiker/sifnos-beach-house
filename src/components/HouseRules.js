71eb5b42756fa43e9ec1e6c1972cfdb3dc5ee993
Lauren-Nicole <popich.red@gmail.com>
2022-09-30 09:28:11 +0000

    Update headings to strong text - HouseRules.js

--- src/components/HouseRules.js
+++ src/components/HouseRules.js
@@ -7,7 +7,7 @@ function HouseRules() {
             <h3>Things to know</h3>
             <Row>
                 <Col xs={12} sm={4}>
-                    <h4>House Rules:</h4>
+                    <h4><strong>House Rules:</strong></h4>
                     <p><i className="fa-regular fa-clock" /> Check-in: 16:00 to 21:00</p>
                     <p><i className="fa-regular fa-clock" /> Check-out: 10:00</p>
                     <p><i className="fa-solid fa-ban-smoking" /> No Smoking</p>
@@ -16,13 +16,13 @@ function HouseRules() {
                 </Col>
 
                 <Col xs={12} sm={4}>
-                    <h4>Health & Safety:</h4>
+                    <h4><strong>Health & Safety:</strong></h4>
                     <p><i className="fa-solid fa-circle-exclamation" /> Carbon Monoxide Detector: No</p>
                     <p><i className="fa-solid fa-circle-exclamation" /> Smoke Alarm: No</p>
                 </Col>
 
                 <Col xs={12} sm={4}>
-                    <h4>Cancellation Policy:</h4>
+                    <h4><strong>Cancellation Policy:</strong></h4>
                     <p>Full refund within 48h from the time the reservation was made.</p>
                     <p>+ 30 days: 50% refund</p>
                     <p>30 days or less: No refund.</p>
