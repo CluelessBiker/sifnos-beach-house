5090b9252c58a2bf5355409c754e4e8ff57717c7
Lauren-Nicole <popich.red@gmail.com>
2022-10-03 07:40:26 +0000

    Import ContactForm.js, remove unused imports - Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,93 +1,14 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import styles from '../styles/Contact.module.css';
-import { useHistory } from 'react-router-dom';
 import pdf from '../assets/documents/leasecontract.pdf';
+import ContactForm from '../components/ContactForm';
 
-export const Contact = () => {
-    const form = useRef();
-    const history = useHistory();
-
-    const sendEmail = (e) => {
-        e.preventDefault();
-
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-        .then((result) => {
-            console.log(result.text); 
-        }, (error) => {
-            console.log(error.text);
-        });
-        history.push('/');
-    };
+function Contact() {
 
     return (
         <Row>
-            <Col sm={12} md={8} className={styles.ContactForm}>
-                <h3 className="contact-heading uppercase text-center">Get in touch</h3>
-                <Row>
-                    <Col className={styles.Form}>
-                        <form ref={form} onSubmit={sendEmail}>
-                            <Row>
-                                <input
-                                    type="text"
-                                    name="from_name"
-                                    placeholder="full name*"
-                                    required
-                                />
-
-                                <input
-                                    type="email"
-                                    name="from_email"
-                                    placeholder="email*"
-                                    required
-                                />
-                                <input
-                                    type="tel"
-                                    name="from_number"
-                                    placeholder="phone number*"
-                                    required
-                                />
-                            </Row>
-                            <Row>
-                                <input
-                                    type="date"
-                                    name="arrival_date"
-                                    placeholder="arrival*"
-                                    required
-                                />
-                                <input
-                                    type="date"
-                                    name="departure_date"
-                                    placeholder="departure*"
-                                    required
-                                />
-                                <input
-                                    type="number"
-                                    name="number_of_guests"
-                                    placeholder="number of guests*"
-                                    required
-                                />
-                            </Row>
-                            <textarea
-                                rows="5"
-                                name="message"
-                                id="projectsummary"
-                                className="form-control"
-                                placeholder="message*"
-                                required
-                            ></textarea>
-                            <Col>
-                                <button
-                                    type="submit"
-                                    className="btn btn-secondary"
-                                    value="send"
-                                >Send</button>
-                            </Col>
-                        </form>
-                    </Col>
-                </Row>
-            </Col>
+            <ContactForm />
             <Col sm={12} md={4} className={styles.Fees}>
                 <div className={styles.FeesBox}>
                     <Row className={styles.ServiceCost}>
