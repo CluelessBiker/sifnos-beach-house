c159d84cfe940beb341f1da5811868fd1467fd1d
Lauren-Nicole <popich.red@gmail.com>
2022-12-22 13:43:53 +0000

    feat: UI component for external links.

---
+++ src/components/SiteLinks.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import styles from '../styles/SiteLinks.module.css';
+
+export const NavbarLinks = (props, ...rest) => {
+    return (
+        <NavLink
+            exact
+            to={props.path}
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            aria-label={props.page}
+            rel="noreferrer"
+        >
+            {props.page}
+        </NavLink>
+    )
+}
+
+export const SocialLinks = (props, ...rest) => {
+    return (
+        <a
+            href={props.link}
+            target="_blank"
+            rel="noreferrer"
+            aria-label={`Visit our ${props.socialsite} page`}
+        >
+            {props.icon}
+        </a>
+    )
+}
