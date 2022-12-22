a26ad54b85363e67fee38060e629abdd45daa028
Lauren-Nicole <popich.red@gmail.com>
2022-12-22 15:06:28 +0000

    refactor: remove heading, rows & cells.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,11 @@
 /* eslint-disable */
-import React from 'react'
+import React from 'react';
 
 const Table = (props) => {
     return (
-        <>
         <table>
-            <tr>
-            {props.rowCells.map((cell, index) => (
-                <td key={index}><p>{cell}</p></td>
-            ))}
-            </tr>
+            {props.children}
         </table>
-
-        </>
     )
 }
 
