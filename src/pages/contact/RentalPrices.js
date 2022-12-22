14cb16da9da1128996b4edf6c90d0dbd22dcd353
Lauren-Nicole <popich.red@gmail.com>
2022-12-22 15:08:17 +0000

    update: add table compoents.

--- src/pages/contact/RentalPrices.js
+++ src/pages/contact/RentalPrices.js
@@ -2,9 +2,11 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import Table from '../../components/Table';
 import styles from '../../styles/RentalPrices.module.css';
 import pricingdata from '../../assets/data/pricingdata';
+import Table from '../../components/Table';
+import TableHead from '../../components/TableHead';
+import TableRow from '../../components/TableRow';
 
 const rows = pricingdata;
     
@@ -13,19 +15,19 @@ const createData = (row) => {
 };
 
 function RentalPrices() {
-    
-    
-
     return (
         <Col sm={12} md={4} className={styles.Fees}>
             <div className={styles.FeesBox}>
-                <h3>Pricing</h3> 
+                <Table>
+                    <TableHead heading={"Pricing"}/>
+                    {rows.map(row => (
+                        <TableRow
+                            rowCells={createData(row)}
+                        />
+                    ))}
+                </Table> 
+              
 
-                {rows.map(row => (
-                    <Table
-                        rowCells={createData(row)}
-                    />
-                ))}
                 <p>*5 night minimum</p>
 
                 <Link to="/lease" target="_blank"><i className="fa-solid fa-download" aria-hidden="true" /> Rental Contract</Link>
