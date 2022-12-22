f9b564538b750e8ff3fa4d44bbc3f6ce1b3ca153
Lauren-Nicole <popich.red@gmail.com>
2022-12-22 13:28:58 +0000

    docs: add data array for rentals costs.

---
+++ src/assets/data/pricingdata.js
@@ -0,0 +1,20 @@
+const pricingdata = [
+    {
+        service: '€799 x 5 nights',
+        amount: '€3,995',
+    },
+    {
+        service: 'Cleaning fee',
+        amount: '€195',
+    },
+    {
+        service: 'Deposit',
+        amount: '€1,500',
+    },
+    {
+        service: 'Total',
+        amount: '€5,690',
+    },
+]
+
+export default pricingdata
