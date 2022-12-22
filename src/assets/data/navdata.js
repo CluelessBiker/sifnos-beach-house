471a2f562daa14bcbf5f37868a274fd45cbe6627
Lauren-Nicole <popich.red@gmail.com>
2022-12-22 13:28:01 +0000

    docs: add data array for navigation links.

---
+++ src/assets/data/navdata.js
@@ -0,0 +1,24 @@
+const navdata = [
+    {
+        page: 'Home',
+        path: '/',
+    },
+    {
+        page: 'Accomodation',
+        path: '/accommodation',
+    },
+    {
+        page: 'Neighborhood',
+        path: '/neighborhood',
+    },
+    {
+        page: 'Gallery',
+        path: '/gallery',
+    },
+    {
+        page: 'Contact',
+        path: '/contact',
+    },
+]
+
+export default navdata
