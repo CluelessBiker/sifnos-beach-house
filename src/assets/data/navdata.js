c424dd5b8b136e05b985f832d9b43bd44526c5ac
Lauren-Nicole <popich.red@gmail.com>
2022-12-28 15:02:56 +0000

    fix: add id field to object - navdata.

--- src/assets/data/navdata.js
+++ src/assets/data/navdata.js
@@ -1,21 +1,26 @@
 const navdata = [
     {
+        id: 1,
         page: 'Home',
         path: '/',
     },
     {
+        id: 2,
         page: 'Accomodation',
         path: '/accommodation',
     },
     {
+        id: 3,
         page: 'Neighborhood',
         path: '/neighborhood',
     },
     {
+        id: 4,
         page: 'Gallery',
         path: '/gallery',
     },
     {
+        id: 5,
         page: 'Contact',
         path: '/contact',
     },
