9e1727962e77cb10e153fd42abd6c7a3a0a788aa
Lauren-Nicole <popich.red@gmail.com>
2022-12-28 15:08:26 +0000

    fix: Re-organise file - thead.

---
+++ src/components/tables/TableHead.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import React from 'react'
+
+const TableHead = (props) => {
+    return (
+        <tr>
+            <th colSpan="2"><h3>{props.heading}</h3></th>
+        </tr>
+    )
+}
+
+export default TableHead
