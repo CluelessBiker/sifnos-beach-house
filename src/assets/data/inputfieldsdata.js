3caf10f7a30e0e636b59469c66d6ff3c8cf592e5
Lauren-Nicole <popich.red@gmail.com>
2022-12-28 15:02:01 +0000

    feat: add array for input fields.

---
+++ src/assets/data/inputfieldsdata.js
@@ -0,0 +1,40 @@
+const fields = [
+    {
+        id: 1,
+        type: "text",
+        name: "from_name",
+        placeholder: "full name*",
+    },
+    {
+        id: 2,
+        type: "email",
+        name: "from_email",
+        placeholder: "email*",
+    },
+    {
+        id: 3,
+        type: "tel",
+        name: "from_number",
+        placeholder: "phone number*",
+    },
+    {
+        id: 4,
+        type: "number",
+        name: "number_of_guests",
+        placeholder: "number of guests*",
+    },
+    {
+        id: 5,
+        type: "date",
+        name: "arrival_date",
+        placeholder: "arrival*",
+    },
+    {
+        id: 6,
+        type: "date",
+        name: "departure_date",
+        placeholder: "departure*",
+    },
+]
+
+export default fields
