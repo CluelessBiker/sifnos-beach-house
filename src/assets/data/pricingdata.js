1f74188b6a3ca36b3419c8176d64a70b08935c42
Lauren-Nicole <popich.red@gmail.com>
2022-12-28 15:03:12 +0000

    fix: add id field to object - pricing data

--- src/assets/data/pricingdata.js
+++ src/assets/data/pricingdata.js
@@ -1,17 +1,21 @@
 const pricingdata = [
     {
+        id: 1,
         service: 'Daily rate',
         amount: '€799',
     },
     {
+        id: 2,
         service: 'Cleaning fee',
         amount: '€195',
     },
     {
+        id: 3,
         service: 'Deposit',
         amount: '€1,500',
     },
     // {
+    //     id: 4,
     //     service: 'Total',
     //     amount: '€5,690',
     // },
