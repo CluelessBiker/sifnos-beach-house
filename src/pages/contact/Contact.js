6ff2e040457791a5dae601d4a04f90094765b211
Lauren-Nicole <popich.red@gmail.com>
2023-01-02 12:49:59 +0000

    fix: adjust spacing of column panels - contact

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import ContactForm from './components/ContactForm';
 import RentalPrices from './components/RentalPrices';
 
@@ -9,8 +9,13 @@ function Contact() {
         <div>
             <br />
             <Row>
-                <ContactForm />
-                <RentalPrices />
+                <Col sm={12} md={6}>
+                    <ContactForm />
+                </Col>
+
+                <Col sm={12} md={6}>
+                    <RentalPrices />
+                </Col>
             </Row>
         </div>
     );
